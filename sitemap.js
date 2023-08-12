37306bf9cd3601cb7e19b0e8bb3f7f5b5f8364c8
Shamaz saeed <shamazsaeed332@gmail.com>
2023-08-12 15:12:42 +0500

    final

---
+++ sitemap.js
@@ -0,0 +1,567 @@
+const  a =[
+  {
+    "name": "Studio",
+    "tid": "7973",
+    "parent": "0",
+    "arabic": "ستوديو",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7645"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "1",
+    "tid": "7974",
+    "parent": "0",
+    "arabic": "1",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7946"
+      },
+      {
+        "target_id": "7670"
+      }
+    ]
+  },
+  {
+    "name": "2",
+    "tid": "7975",
+    "parent": "0",
+    "arabic": "2",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "3",
+    "tid": "7976",
+    "parent": "0",
+    "arabic": "3",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "4",
+    "tid": "7977",
+    "parent": "0",
+    "arabic": "4",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "5",
+    "tid": "7978",
+    "parent": "0",
+    "arabic": "5",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "6",
+    "tid": "7979",
+    "parent": "0",
+    "arabic": "6",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "7",
+    "tid": "7980",
+    "parent": "0",
+    "arabic": "7",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7659"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "7+",
+    "tid": "7981",
+    "parent": "0",
+    "arabic": "7+",
+    "cl_categories": [
+      {
+        "target_id": "7642"
+      },
+      {
+        "target_id": "7644"
+      },
+      {
+        "target_id": "7658"
+      },
+      {
+        "target_id": "7670"
+      },
+      {
+        "target_id": "7946"
+      }
+    ]
+  },
+  {
+    "name": "8",
+    "tid": "7982",
+    "parent": "0",
+    "arabic": "8",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "9",
+    "tid": "7983",
+    "parent": "0",
+    "arabic": "9",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "10",
+    "tid": "7984",
+    "parent": "0",
+    "arabic": "10",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "11",
+    "tid": "7985",
+    "parent": "0",
+    "arabic": "11",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "12",
+    "tid": "7986",
+    "parent": "0",
+    "arabic": "12",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "13",
+    "tid": "7987",
+    "parent": "0",
+    "arabic": "13",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "14",
+    "tid": "7988",
+    "parent": "0",
+    "arabic": "14",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "15",
+    "tid": "7989",
+    "parent": "0",
+    "arabic": "15",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "15+",
+    "tid": "7990",
+    "parent": "0",
+    "arabic": "15+",
+    "cl_categories": [
+      {
+        "target_id": "7643"
+      },
+      {
+        "target_id": "7659"
+      }
+    ]
+  },
+  {
+    "name": "Single",
+    "tid": "7991",
+    "parent": "0",
+    "arabic": "فردية",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "Double",
+    "tid": "7992",
+    "parent": "0",
+    "arabic": "مزدوجة",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "Connecting",
+    "tid": "7993",
+    "parent": "0",
+    "arabic": "متواصلة",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "1 Bed Apartment",
+    "tid": "7994",
+    "parent": "0",
+    "arabic": "شقة ب 1 غرفة نوم",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "2 Bed Apartment",
+    "tid": "7995",
+    "parent": "0",
+    "arabic": "شقة ب 2 غرفة نوم",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "3 Bed Apartment",
+    "tid": "7996",
+    "parent": "0",
+    "arabic": "شقة ب 3 غرفة نوم",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "4 Bed Apartment",
+    "tid": "8589",
+    "parent": "0",
+    "arabic": "شقة ب 4 غرفة نوم",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "Master",
+    "tid": "7997",
+    "parent": "0",
+    "arabic": "غرفة ماستر",
+    "cl_categories": [
+      {
+        "target_id": "7645"
+      }
+    ]
+  },
+  {
+    "name": "Private",
+    "tid": "7998",
+    "parent": "0",
+    "arabic": "خاصة",
+    "cl_categories": [
+      {
+        "target_id": "7647"
+      }
+    ]
+  },
+  {
+    "name": "Shared",
+    "tid": "7999",
+    "parent": "0",
+    "arabic": "مشتركة",
+    "cl_categories": [
+      {
+        "target_id": "7647"
+      }
+    ]
+  },
+  {
+    "name": "1-20",
+    "tid": "8000",
+    "parent": "0",
+    "arabic": "1-20",
+    "cl_categories": [
+      {
+        "target_id": "7653"
+      },
+      {
+        "target_id": "7948"
+      },
+      {
+        "target_id": "7666"
+      }
+    ]
+  },
+  {
+    "name": "21-50",
+    "tid": "8001",
+    "parent": "0",
+    "arabic": "21-50",
+    "cl_categories": [
+      {
+        "target_id": "7653"
+      },
+      {
+        "target_id": "7948"
+      },
+      {
+        "target_id": "7666"
+      }
+    ]
+  },
+  {
+    "name": "51-100",
+    "tid": "8002",
+    "parent": "0",
+    "arabic": "51-100",
+    "cl_categories": [
+      {
+        "target_id": "7653"
+      },
+      {
+        "target_id": "7948"
+      },
+      {
+        "target_id": "7666"
+      }
+    ]
+  },
+  {
+    "name": "101+",
+    "tid": "8003",
+    "parent": "0",
+    "arabic": "101+",
+    "cl_categories": [
+      {
+        "target_id": "7653"
+      },
+      {
+        "target_id": "7948"
+      },
+      {
+        "target_id": "7666"
+      }
+    ]
+  }
+]
+  const friendlyUrlString = (str) => {
+    str = str.replace(/[ \/]/g, "-");
+    str = str.replace("&-", "");
+    return str.toLowerCase();
+  };
+
+
+
+    const filterName = a.map((item) => {
+        return item.tid
+    }
+    )
+
+    console.log(filterName)
+
